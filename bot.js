df1d06541eeb13a5b571f92054afe5bfd0bc7238
prathamhere5-glitch <prathamhere5@gmail.com>
2025-10-26 19:10:47 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Replace with your real Telegram token and chat ID
-const TELEGRAM_TOKEN = 'YOUR_TELEGRAM_TOKEN_HERE';
-const TELEGRAM_CHAT_ID = 'YOUR_TELEGRAM_CHAT_ID_HERE';
+const TELEGRAM_TOKEN = '8111876690:AAETmnCuSI71NXKiCI2VpgtoQiTq5sVliDw';
+const TELEGRAM_CHAT_ID = '6729390752';
 
 const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 
